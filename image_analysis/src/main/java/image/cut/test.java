3cd64498399943d926787e219bb2f161f92aefed
litianwei <3245849061@qq.com>
2023-11-30 22:03:30 +0800

    project test

---
+++ image_analysis/src/main/java/image/cut/test.java
@@ -0,0 +1,14 @@
+package image.cut;
+
+/**
+ * test
+ *
+ * @Author litianwei
+ * @Date 2023/11/30
+ **/
+public class test {
+
+    public test(String[] args) {
+        System.out.println("test!");
+    }
+}
